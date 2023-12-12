ab4060130e56561dbbc3e1f687ee1df04a737795
aristides1000 <arsitides1000@gmail.com>
2023-12-12 16:01:02 -0400

    Feat: finish index.php

--- index.php
+++ index.php
@@ -34,5 +34,51 @@
         </form>
       </div>
     </div>
+
+    <div class="col-md-8 mb-5">
+      <table class="table table-bordered">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Created At</th>
+            <th>Image</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <?php
+          $query = "SELECT * FROM task";
+          $result_tasks = mysqli_query($conn, $query);
+
+          while($row = mysqli_fetch_assoc($result_tasks)) {
+          ?>
+            <tr>
+              <td><?php echo $row['title']; ?></td>
+              <td><?php echo $row['description']; ?></td>
+              <td><?php echo $row['created_at']; ?></td>
+              <td class="resize-image">
+                <img src="image.php?id=<?php echo $row['id'] ?>" alt="image" class="w-100">
+              </td>
+              <td>
+                <a href="edit.php?id=<?php echo $row['id'] ?>" class="btn btn-secondary rounded-circle">
+                  <ion-icon name="create"></ion-icon>
+                </a>
+              </td>
+              <td>
+                <a href="delete_task.php?id=<?php echo $row['id'] ?>" class="btn btn-danger rounded-circle">
+                  <ion-icon name="trash"></ion-icon>
+                </a>
+              </td>
+            </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+    </div>
   </div>
 </main>
+
+<?php
+  include("includes/footer.php");
+?>
