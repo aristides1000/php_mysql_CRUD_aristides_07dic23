6f66f5348ade278bbd18357cefc0e9df8aa99521
aristides1000 <arsitides1000@gmail.com>
2023-12-13 16:08:53 -0400

    Feat: finish header

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>To Do List</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header class="fixed-header">
+    <nav class="navbar bg-body-tertiary">
+      <div class="container-fluid">
+        <a href="index.php" class="navbar-brand align-elements">
+          <i class="bi bi-bell-fill me-3"></i>
+          <span>Pagina de tareas</span>
+        </a>
+      </div>
+    </nav>
+  </header>
