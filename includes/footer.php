8794df3f75cdb067b3f7bead88e7d88cdb36ba00
aristides1000 <arsitides1000@gmail.com>
2023-12-14 16:02:33 -0400

    Feat: finish folders includes and database

--- includes/footer.php
+++ includes/footer.php
@@ -1,10 +1,35 @@
-<div class="container-fluid fixed-footer"></div>
-  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top background-color-transparency">
-    <div class="col-md-4 d-flex align-items-center">
-      <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-        <i class="bi bi-bootstrap-fill ms-4 icon-size"></i>
-      </a>
-      <span class="mb-3 mb-md-0 text-body-secondary">© 2023 Company</span>
-    </div>
-  </footer>
-</div>
+  <div class="container-fluid fixed-footer"></div>
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top background-color-transparency">
+      <div class="col-md-4 d-flex align-items-center">
+        <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+          <i class="bi bi-bootstrap-fill ms-4 icon-size"></i>
+        </a>
+        <span class="mb-3 mb-md-0 text-body-secondary">© 2023 Company</span>
+      </div>
+
+      <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+        <li class="ms-3">
+          <a href="#" class="text-body-secondary">
+            <i class="bi bi-twitter-x"></i>
+          </a>
+        </li>
+        <li class="ms-3">
+          <a href="#" class="text-body-secondary">
+            <i class="bi bi-instagram"></i>
+          </a>
+        </li>
+        <li class="ms-3">
+          <a href="#" class="text-body-secondary">
+            <i class="bi bi-facebook"></i>
+          </a>
+        </li>
+      </ul>
+    </footer>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+  <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
