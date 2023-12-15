073e8f62d9aa22d6f2d8144867d5f736911c0ad4
aristides1000 <arsitides1000@gmail.com>
2023-12-15 16:53:27 -0400

    Feat: add somes files

---
+++ delete_task.php
@@ -0,0 +1,17 @@
+<?php
+  include("database/db.php");
+
+  if(isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $query = "DELETE FROM task WHERE id = $id";
+    $result = mysqli_query($conn, $query);
+
+    if(!$result) {
+      die("Query Failed.");
+    }
+
+    $_SESSION['message'] = 'Task Removed Successfully';
+    $_SESSION['message'] = 'danger';
+    header('Location: index.php');
+  }
+?>
---
+++ save_task.php
@@ -0,0 +1,26 @@
+<?php
+  include('database/db.php');
+
+  if(isset($_POST['save_task'])) {
+    $title = $POST['title'];
+    $description = $POST['description'];
+    $image = getimagesize($_FILES['image']['tmp_name']);
+    if($image !== false) {
+      $image = $_FILES['image']['tmp_name'];
+      $img_content = addslashes(file_get_contents($image));
+    } else {
+      $img_content = "";
+    }
+
+    $query = "INSERT INTO task(title, description, image) VALUES ('$title', '$description', '$img_content')";
+    $result = mysqli_query($conn, $query);
+
+    if(!$result) {
+      die("Query failed");
+    }
+
+    $_SESSION['message'] = 'Task Saved Successfully';
+    $_SESSION['message_type'] = 'success';
+    header('Location: index.php');
+  }
+?>
